ee8f53f9c23504f63c017dbd3b5d9c672343dd6e
rswang47 <rswang47@gmail.com>
2018-11-08 12:00:19 -0800

    Update crawl_twitch.py

--- crawler/crawl_twitch.py
+++ crawler/crawl_twitch.py
@@ -9,12 +9,11 @@
 
 
 
-import urllib2
+import urllib.request as urllib2
 import time
 import datetime
 import os
 import inspect
-import urllib
 import threading
 import zipfile
 
@@ -25,32 +24,42 @@ PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
 from os import listdir
 from os.path import isfile, join
-files_dict = dict()	
+files_dict = dict()
+CLIENT_ID = 'urfntwbl92o0ojm7hee04t4tbfmavm'
 
 class multi_crawler(threading.Thread):
-    def run(self):
-        global total_html
-        kwargs = self._Thread__kwargs
-        n = kwargs["n"]
-        url = kwargs["url"] + str(n) + '&' + 'broadcaster_software=&' + 'on_site=1'
-        # print n
-        if not total_html.has_key(n):
-	        s = urllib.urlopen(url).read()
-	        total_html[n] = s + '\n'
+	def __init__(self, **kwargs):
+
+		threading.Thread.__init__(self)
+		self.kwargs = kwargs['kwargs']
+
+	def run(self):
+		global total_html
+		n = self.kwargs["n"]
+		url = self.kwargs["url"] + str(n) + '&' + 'broadcaster_software=&' + 'on_site=1'
+		request = urllib2.Request(url)
+		request.add_header('Client-ID', CLIENT_ID)
+		# print n
+		if not n in total_html.keys():
+			s = urllib2.urlopen(request).read()
+			s = s.decode('utf-8')
+			total_html[n] = s + '\n'
 
 
 
 
 def return_total(html):
-	flag = html.split('_total":')[1]
+	flag = html.split(b'\"_total\":')[1]
 	if flag:
-		flag_2 = flag.split(',\"_links\"')[0]
+		flag_2 = flag.split(b',\"streams\"')[0]
 		if flag_2:
 			return int(flag_2)
 
 def return_html(start_num):
-	twitch_url = 'http://api.twitch.tv/kraken/streams?limit=100&offset=' + str(start_num) + '&broadcaster_software=&on_site=1'
-	response = urllib2.urlopen(twitch_url)
+	twitch_url = 'https://api.twitch.tv/kraken/streams?limit=100&offset=' + str(start_num) + '&broadcaster_software=&on_site=1'
+	twitch_request = urllib2.Request(twitch_url)
+	twitch_request.add_header('Client-ID', CLIENT_ID)
+	response = urllib2.urlopen(twitch_request)
 	return response.read()
 
 def save_total(platform):
@@ -68,7 +77,7 @@ def save_total(platform):
 		total = return_total(html)
 		fw2.writelines(platform + '-' + date + ':' + str(total) + '\n')
 		fw2.close()
-		works = [multi_crawler(kwargs={"url": "http://api.twitch.tv/kraken/streams?limit=100&offset=", "n": i}) for i in range(0,total,100)]
+		works = [multi_crawler(kwargs={"url": "https://api.twitch.tv/kraken/streams?limit=100&offset=", "n": i}) for i in range(0,total,100)]
 		# print 'start'
 		num_works = len(works) 
 		start_works = 0
@@ -77,21 +86,20 @@ def save_total(platform):
 			    works[i].start()
 			# print 'join'
 			for i in range(start_works, min(start_works + 50, num_works)):
-	   			works[i].join()
-	   		start_works += 50
-   		for i in range(0,total,100):
+				works[i].join()
+			start_works += 50
+		for i in range(0,total,100):
 			fw.writelines(total_html[i])
 		fw.close()
 		zf = zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED)
 		zf.write(file_path, txt_name)
 		zf.close()
 		os.remove(file_path)
-	except BaseException, e:
+	except BaseException as e:
 		error = str(e)
+		print(error)
 	
 
 total_html = dict()
 save_total("all")
-
-
 
