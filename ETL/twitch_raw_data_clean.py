63c6facac77ad5aabbac75642471155ac0c7f724
rswang47 <rswang47@gmail.com>
2018-11-29 14:33:32 -0800

    Update twitch_raw_data_clean.py

--- ETL/twitch_raw_data_clean.py
+++ ETL/twitch_raw_data_clean.py
@@ -2,8 +2,8 @@ from pyspark.sql import SparkSession, functions, types
 import sys
 import json
 
-def main():
-	df = spark.read.json('all-2018-11-08-11-55-01.txt')
+def main(filename):
+	df = spark.read.json(filename)
 	data = df.select(functions.explode('streams').alias('streams_info')).select('streams_info.*')
 	data_s = data.select(
 		data['_id'].alias('stream_id'), data['game'], data['viewers'], data['video_height'],
@@ -13,8 +13,8 @@ def main():
 		'channel.status', 'channel.broadcaster_language', 'channel.language', 'channel.broadcaster_software',
 		'channel.created_at', 'channel.updated_at', 'channel.mature', 'channel.partner')
 
-	data_s.coalesce(1).write.json('stream_base', mode = 'overwrite')
-	data_c.coalesce(1).write.json('channel_base', mode = 'overwrite')
+	data_s.coalesce(10).write.json('stream_base', mode = 'overwrite')
+	data_c.coalesce(10).write.json('channel_base', mode = 'overwrite')
 
 
 
@@ -22,4 +22,6 @@ def main():
 if __name__ == '__main__':
 	spark = SparkSession.builder.appName('clean data').getOrCreate()
 	spark.sparkContext.setLogLevel('WARN')
-	main()
+	
+	filename = sys.argv[1]
+	main(filename)
