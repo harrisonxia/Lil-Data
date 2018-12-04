61f33501aa084c83d274df4f5ae980e4233ffbc9
rswang47 <rswang47@gmail.com>
2018-12-04 13:15:39 -0800

    Update join_with_giantbomb.py

--- ETL/join_with_giantbomb.py
+++ ETL/join_with_giantbomb.py
@@ -2,17 +2,18 @@ from pyspark.sql import SparkSession, functions, types
 import sys
 import json
 
-def join(path):
-	game_info_df = spark.read.json(path + 'game_info_base')
-	game_df = spark.read.json(path + 'game_cleanned')
+def join():
+	stream_df = spark.read.json('stream_cleanned')
+	giant_df = spark.read.json('game_info_base')
 
-	real_game_df = game_df.join(game_info_df, functions.lower(game_df.game) == functions.lower(game_info_df.name), 'left')
-	real_game_df.coalesce(1).write.json('real_game_info', mode = 'overwrite')
-  
+	stream_df = stream_df.withColumn('gen_name', functions.lower(stream_df.game))
+	game_info = stream_df.groupBy('gen_name').count().cache()
+
+	real_game_info = game_info.join(giant_df, game_info.gen_name == functions.lower(giant_df.name), 'left')
+
+	real_game_info.coalesce(1).write.json('real_game_info', mode = 'overwrite')
   
 if __name__ == '__main__':
 	spark = SparkSession.builder.appName('join data').getOrCreate()
 	spark.sparkContext.setLogLevel('WARN')
-
-	path = sys.argv[1]
-	join(path)
+	join()
