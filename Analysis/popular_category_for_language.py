625a206e6a93fb59d86c85b091a777cb5f1300fb
lmbkv <aisik95@gmail.com>
2018-12-07 18:03:55 -0800

    Update popular_category_for_language.py

--- Analysis/popular_category_for_language.py
+++ Analysis/popular_category_for_language.py
@@ -1,29 +1,20 @@
-from pyspark.sql import SparkSession, functions, types
-from pyspark.sql.types import StringType, LongType, IntegerType, BooleanType, FloatType, DateType
 import sys
-assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
-
 import json
-import datetime as dt
-from pyspark.sql.types import DateType
-
-spark = SparkSession.builder.appName('data analysis').getOrCreate()
-assert spark.version >= '2.3' # make sure we have Spark 2.3+
-spark.sparkContext.setLogLevel('WARN')
-
-import matplotlib.pyplot as plt
-
+from pyspark.sql import SparkSession, functions, types
 from pyspark.sql.functions import explode
 
-
-def main():
+def popular_category_for_languages():
     data_stream = spark.read.json('stream_cleanned')
     data_channel = spark.read.json('channel_cleanned')
     data_game = spark.read.json('real_game_info').cache()
     data_genre = spark.read.json('game_genre').cache()
 
-    data_stream.createOrReplaceTempView('data_s')
+    #data_stream = spark.read.json('/user/rishengw/stream_cleanned/')
+    #data_channel = spark.read.json('/user/rishengw/channel_cleanned/')
+    #data_game = spark.read.json('/user/rishengw/real_game_info/').cache()
+    #data_genre = spark.read.json('/user/rishengw/game_genre/').cache()
 
+    data_stream.createOrReplaceTempView('data_s')
     data_channel.createOrReplaceTempView('data_c')
     data_game.createOrReplaceTempView('data_g')
     data_genre.createOrReplaceTempView('data_genre')
@@ -39,7 +30,7 @@ def main():
         """
             )
 
-    #game_with_channel.show()
+    game_with_channel.cache()
 
     #to flatten the categories
     games_with_genres_languages = game_with_channel.select(
@@ -64,8 +55,6 @@ def main():
         """
             ).createOrReplaceTempView('categories_languages_viewers')
 
-    #categories_languages_viewers.show()
-
     #getting most popular category for each language
     popular_category_for_languages= spark.sql(
         """SELECT language, genres, viewers FROM (SELECT c.genres, c.language, c.viewers
@@ -76,12 +65,10 @@ def main():
         ORDER BY language  )     
         """
             )
-    #popular_category_for_languages.show()
+
     popular_category_for_languages.coalesce(1).write.json('popular_category_for_languages', mode = 'overwrite')
 
-
 if __name__ == '__main__':
-    main()
-
-
-
+    spark = SparkSession.builder.appName('data analysis').getOrCreate()
+    spark.sparkContext.setLogLevel('WARN')
+    popular_category_for_languages()
